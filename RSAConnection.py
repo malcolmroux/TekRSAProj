e443e6f1da11d3f231fccc27e9045421b93b2b45
malcolmroux <43730247+malcolmroux@users.noreply.github.com>
2019-01-15 09:46:26 -0800

    Initial commit

---
+++ RSAConnection.py
@@ -0,0 +1,9 @@
+class RSAConnection:
+
+    def __init__(self):
+        # needed initialzations
+        self.port_number = 1234
+        self.running = False
+
+        #init values (testing)
+        self.
